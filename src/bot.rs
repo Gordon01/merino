e63f5f8a929bed40b7225267722182ab213e4eb0
Alexander Sergeev <etc9053@gmail.com>
2022-01-22 19:10:10 -0500

    Teloxide interation. Bot can add ip to whitelist and show rejected ones

--- src/bot.rs
+++ src/bot.rs
@@ -1,7 +1,6 @@
-use crate::Merino;
 use std::collections::HashSet;
 use std::error::Error;
-use std::net::IpAddr;
+use std::net::{IpAddr, Ipv4Addr};
 use std::sync::{Arc, RwLock};
 use teloxide::{prelude::*, utils::command::BotCommand};
 use tokio_stream::wrappers::UnboundedReceiverStream;
@@ -17,18 +16,52 @@ enum Command {
     Add(String),
 }
 
-async fn answer(
+/// Parse the text wrote on Telegram and check if that text is a valid command
+/// or not, then match the command.
+async fn message_handler(
     cx: UpdateWithCx<AutoSend<Bot>, Message>,
-    command: Command,
+    whitelist: Arc<RwLock<HashSet<IpAddr>>>,
+    rejected_addresses: Arc<RwLock<HashSet<IpAddr>>>,
 ) -> Result<(), Box<dyn Error + Send + Sync>> {
-    match command {
-        Command::Help => cx.answer(Command::descriptions()).await?,
-        Command::Rejected => cx.answer("rejected list").await?,
-        Command::Add(username) => {
-            cx.answer(format!("Your username is @{}.", username))
-                .await?
+    match cx.update.text() {
+        Some(text) => {
+            match BotCommand::parse(text, "buttons") {
+                Ok(Command::Help) => {
+                    // Just send the description of all commands.
+                    cx.answer(Command::descriptions()).await?;
+                }
+                Ok(Command::Rejected) => {
+                    cx.answer(format!(
+                        "There are {} rejected addresses:\n{}",
+                        rejected_addresses.read().unwrap().len(),
+                        rejected_addresses
+                            .read()
+                            .unwrap()
+                            .iter()
+                            .map(|a| format!("{}\n", a.to_string()))
+                            .collect::<String>()
+                    ))
+                    .await?;
+                }
+                Ok(Command::Add(ip)) => match ip.parse::<IpAddr>() {
+                    Ok(ip) => {
+                        whitelist.write().unwrap().insert(ip);
+                        cx.answer(format!("IP {} is added to whitelist", ip))
+                            .await?;
+                    }
+                    Err(e) => {
+                        cx.answer(format!("IP cannot be parsed: {}", e))
+                            .await?;
+                    }
+                },
+
+                Ok(_) | Err(_) => {
+                    cx.reply_to("Command not found!").await?;
+                }
+            }
         }
-    };
+        None => {}
+    }
 
     Ok(())
 }
@@ -42,21 +75,16 @@ pub async fn start_bot(
     //let bot_name: String = "merino_bot".to_string();
     //teloxide::commands_repl(bot, bot_name, answer).await;
     Dispatcher::new(bot)
-        .messages_handler(move |rx: DispatcherHandlerRx<AutoSend<Bot>, Message>| {
-            let whitelist = whitelist.clone();
-            let rejected = rejected_addresses.clone();
-            
-            UnboundedReceiverStream::new(rx).for_each_concurrent(None, |message| async move {
-                let white = Arc::clone(&whitelist);
-                message
-                    .answer(format!(
-                        "Whitelist contains {} entries. rejected_addresses contains {} entries.",
-                        whitelist.read().unwrap().len(),
-                        rejected.read().unwrap().len(),
-                    ))
-                    .await
-                    .log_on_error()
-                    .await;
+        .messages_handler(|rx: DispatcherHandlerRx<AutoSend<Bot>, Message>| {
+            UnboundedReceiverStream::new(rx).for_each_concurrent(None, move |cx| {
+                let whitelist = whitelist.clone();
+                let rejected = rejected_addresses.clone();
+                async move {
+                    message_handler(cx, whitelist, rejected)
+                        .await
+                        .log_on_error()
+                        .await;
+                }
             })
         })
         .dispatch()
